fbe24b4e00422bfee25f99a2c269d8564421088a
afzalansari12 <afzalansari12ab@gmail.com>
2026-06-29 12:46:10 +0530

    object and prototype

---
+++ 11_everythinginjs_isobject/script.js
@@ -0,0 +1,16 @@
+let obj ={
+    a : 1,
+    b : "hello"
+}
+
+obj.c="hello im getting added to OBJ";
+console.log(obj);
+
+function fun(){
+    console.log("fun");
+}
+
+fun();
+
+fun.meraObject = "i am a object inside function ";
+console.log(fun.meraObject);
