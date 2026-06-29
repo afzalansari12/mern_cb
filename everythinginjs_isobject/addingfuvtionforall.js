7ec9dd27b6837b3afc56a0c8ca0fb908f6ac6606
afzalansari12 <afzalansari12ab@gmail.com>
2026-06-29 12:42:25 +0530

    prototype and object

---
+++ everythinginjs_isobject/addingfuvtionforall.js
@@ -0,0 +1,12 @@
+Object.prototype.codingblock = function(){
+    console.log("hey")
+}
+
+let a = 20;
+a.codingblock();
+
+let arr= [1,3,3];
+arr.codingblock();
+
+let str ="hello world";
+str.codingblock();
---
+++ everythinginjs_isobject/whatisprototype.js
@@ -0,0 +1,6 @@
+console.log("type of number", typeof(number));
+console.log("type of string", typeof((string)));
+console.log("type of function", typeof (Function));
+console.log("type of array", typeof(Array));
+console.log("type of object", typeof(Object));
+console.log("type of boolean", typeof(Boolean));
